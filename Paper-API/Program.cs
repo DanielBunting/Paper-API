dac43fe1129abbb7e6ae45f0230db39b2ab19a64
DanielBunting <daniel.bunting93@gmail.com>
2024-02-08 05:05:44 +0000

    Updates the entry point.

--- Paper-API/Program.cs
+++ Paper-API/Program.cs
@@ -80,12 +80,8 @@ namespace Paper
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            //}
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
